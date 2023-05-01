9348a8f8d7908e8549692a7713626bc28df70bcb
Ramajedd <40004459+ramajedd@users.noreply.github.com>
2023-05-01 20:04:20 +0700

    update env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,37 @@
-const { parse } = require("pg-connection-string");
+// const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
-  return {
+//   return {
+//     connection: {
+//       client: "postgres",
+//       connection: {
+//         host,
+//         port,
+//         database,
+//         user,
+//         password
+//       },
+//       debug: false,
+//     },
+//   };
+// };
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      schema: 'public', // Not required
+      ssl: {
+        rejectUnauthorized: false, // For self-signed certificates
       },
-      debug: false,
     },
-  };
-};
+    debug: false,
+  },
+});
